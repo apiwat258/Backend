c06cdf74814318dc58b149559a4d1860f6bebe17
apiwat258 <apiwat02042546@gmail.com>
2025-02-26 17:26:38 +0700

    update sql

--- supplychain_db.sql
+++ supplychain_db.sql
@@ -96,6 +96,22 @@ $$;
 ALTER FUNCTION public.generate_userid(role_prefix text) OWNER TO postgres;
 
 --
+-- Name: update_timestamp(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.update_timestamp() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$;
+
+
+ALTER FUNCTION public.update_timestamp() OWNER TO postgres;
+
+--
 -- Name: dairyfactory_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -119,24 +135,20 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.dairyfactory (
     factoryid character varying(255) DEFAULT nextval('public.dairyfactory_id_seq'::regclass) NOT NULL,
-    userid text NOT NULL,
-    username text,
-    companyname text,
+    companyname text NOT NULL,
     address text NOT NULL,
-    city text,
+    district text,
+    subdistrict text,
     province text,
-    country character varying(255) DEFAULT 'Thailand'::character varying NOT NULL,
+    country character varying(255) DEFAULT 'Thailand'::character varying,
     postcode text,
-    email text,
-    telephone text,
+    telephone text NOT NULL,
     lineid text,
     facebook text,
     location_link text,
     createdon timestamp with time zone,
-    factory_id character varying(255) DEFAULT nextval('public.dairyfactory_id_seq'::regclass),
-    company_name text,
-    line_id text,
-    created_on timestamp with time zone DEFAULT CURRENT_TIMESTAMP
+    wallet_address text NOT NULL,
+    email text NOT NULL
 );
 
 
@@ -185,21 +197,21 @@ ALTER SEQUENCE public.farmer_id_seq OWNER TO postgres;
 
 CREATE TABLE public.farmer (
     farmerid character varying(255) DEFAULT nextval('public.farmer_id_seq'::regclass) NOT NULL,
-    userid text NOT NULL,
-    farmer_name text,
-    companyname text,
+    companyname text NOT NULL,
     address text NOT NULL,
-    city text,
+    district text,
+    subdistrict text,
     province text,
-    country character varying(255) DEFAULT 'Thailand'::character varying NOT NULL,
+    country character varying(255) DEFAULT 'Thailand'::character varying,
     postcode text,
-    email text,
-    telephone text,
+    telephone text NOT NULL,
     lineid text,
     facebook text,
     location_link text,
     createdon timestamp with time zone,
-    wallet_address text NOT NULL
+    wallet_address text NOT NULL,
+    email text NOT NULL,
+    entityid text NOT NULL
 );
 
 
@@ -225,20 +237,20 @@ ALTER SEQUENCE public.logistics_id_seq OWNER TO postgres;
 
 CREATE TABLE public.logisticsprovider (
     logisticsid character varying(255) DEFAULT nextval('public.logistics_id_seq'::regclass) NOT NULL,
-    userid text,
-    companyname text,
-    telephone text,
-    createdon timestamp with time zone,
+    companyname text NOT NULL,
     address text NOT NULL,
-    city text,
+    district text,
+    subdistrict text,
     province text,
-    country text DEFAULT 'Thailand'::text,
+    country character varying(255) DEFAULT 'Thailand'::character varying,
     postcode text,
-    email text,
+    telephone text NOT NULL,
     lineid text,
     facebook text,
     location_link text,
-    username text NOT NULL
+    createdon timestamp with time zone,
+    wallet_address text NOT NULL,
+    email text NOT NULL
 );
 
 
@@ -283,20 +295,20 @@ ALTER SEQUENCE public.retailer_id_seq OWNER TO postgres;
 
 CREATE TABLE public.retailer (
     retailerid character varying(255) DEFAULT nextval('public.retailer_id_seq'::regclass) NOT NULL,
-    userid text,
-    companyname text,
-    telephone text,
-    createdon timestamp with time zone,
+    companyname text NOT NULL,
     address text NOT NULL,
-    city text,
+    district text,
+    subdistrict text,
     province text,
-    country text DEFAULT 'Thailand'::text,
+    country character varying(255) DEFAULT 'Thailand'::character varying,
     postcode text,
-    email text,
+    telephone text NOT NULL,
     lineid text,
     facebook text,
     location_link text,
-    username text NOT NULL
+    createdon timestamp with time zone,
+    wallet_address text NOT NULL,
+    email text NOT NULL
 );
 
 
@@ -335,13 +347,15 @@ ALTER SEQUENCE public.user_seq OWNER TO postgres;
 --
 
 CREATE TABLE public.users (
-    userid character varying(255) DEFAULT public.generate_userid() NOT NULL,
-    email text,
-    password text,
-    role text,
+    userid character varying(255) DEFAULT gen_random_uuid() NOT NULL,
+    username text NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL,
+    role text NOT NULL,
+    entityid text NOT NULL,
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
-    deleted_at timestamp with time zone
+    deleted_at timestamp without time zone
 );
 
 
@@ -351,7 +365,7 @@ ALTER TABLE public.users OWNER TO postgres;
 -- Data for Name: dairyfactory; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.dairyfactory (factoryid, userid, username, companyname, address, city, province, country, postcode, email, telephone, lineid, facebook, location_link, createdon, factory_id, company_name, line_id, created_on) FROM stdin;
+COPY public.dairyfactory (factoryid, companyname, address, district, subdistrict, province, country, postcode, telephone, lineid, facebook, location_link, createdon, wallet_address, email) FROM stdin;
 \.
 
 
@@ -367,8 +381,7 @@ COPY public.externalid (externalid, factoryid, logisticname, sendername, logisti
 -- Data for Name: farmer; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.farmer (farmerid, userid, farmer_name, companyname, address, city, province, country, postcode, email, telephone, lineid, facebook, location_link, createdon, wallet_address) FROM stdin;
-FA2500056	2500043	Apiwat Bunyasartpan	Mark Farm	92/395, lakhok muangeak	Kamphaeng Phet	Kamphaeng Phet	Thailand	12000	test@gmail.com	+66 0634371654	\N	\N	\N	2025-02-15 02:50:48.201208+07	0xCF74E3c1769bD376BB0CDf601DD3B081F2f8B0A6
+COPY public.farmer (farmerid, companyname, address, district, subdistrict, province, country, postcode, telephone, lineid, facebook, location_link, createdon, wallet_address, email, entityid) FROM stdin;
 \.
 
 
@@ -376,7 +389,7 @@ FA2500056	2500043	Apiwat Bunyasartpan	Mark Farm	92/395, lakhok muangeak	Kamphaen
 -- Data for Name: logisticsprovider; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.logisticsprovider (logisticsid, userid, companyname, telephone, createdon, address, city, province, country, postcode, email, lineid, facebook, location_link, username) FROM stdin;
+COPY public.logisticsprovider (logisticsid, companyname, address, district, subdistrict, province, country, postcode, telephone, lineid, facebook, location_link, createdon, wallet_address, email) FROM stdin;
 \.
 
 
@@ -385,14 +398,6 @@ COPY public.logisticsprovider (logisticsid, userid, companyname, telephone, crea
 --
 
 COPY public.organiccertification (certificationid, certificationtype, certificationcid, effective_date, issued_date, created_on, entityid, entitytype, blockchain_tx) FROM stdin;
-EVENT-1739303823	Organic	QmCertification123	2026-06-01 07:00:00+07	2024-06-01 07:00:00+07	2025-02-12 02:57:03.566626+07	FARM001	Farmer	0xb52b648fbb1d5156d7a33057f5a47ad5ce8ed05194df07348ed5672f188e7716
-EVENT-1739304278	Organic	QmSz4an3jPbXre7Y3voKFierDiiXZ5M6eTUqy1SMgGpMjS	2026-06-01 07:00:00+07	2025-02-07 07:00:00+07	2025-02-12 03:04:39.031691+07	2500042	Farmer	0x559ed116202ad72f17f8c299c6243e66ff89bd8e2d0762d071a4091c60976cc5
-EVENT-1739313952	Organic	QmWN1GPFzRAL7KbYqGyHBky1ve29Y2oBTD78Bqhzdpmhb4	2026-06-01 07:00:00+07	2025-02-07 07:00:00+07	2025-02-12 05:45:52.884607+07	FA2500043	Farmer	0x7f0da4417db9f2915b5a62c7ef61f4e9072d9c11b4ebc123bf1c2ba5206a89cb
-EVENT-1739316187	Organic	QmTq37LUDjZWryBjMrtUXfdRffGcR3U2eT3WeyVh1eKeEm	2026-06-01 07:00:00+07	2025-02-07 07:00:00+07	2025-02-12 06:23:07.359519+07	DF2500023	Factory	0xa583520ca2a332574cd17def537de33e817ab0295f80ab1bbb64c6107fa884bf
-EVENT-1739317467	Organic	QmdCJUzhqsJCbPUG4hBgopx1o5BDvMumriyy2iB4E27Ejv	2026-06-01 07:00:00+07	2025-02-07 07:00:00+07	2025-02-12 06:44:27.219699+07	RT2500003	Retailer	0x6a7c7bde57e148b3923358f73cce5ede42b7cea7ae2df17e9772249fea3f7f47
-EVENT-1739319085	Organic	QmSE6s4sDT8giJFTkubWkNNxNovcqMBHWLmZyJhrisQRsi	2026-06-01 07:00:00+07	2025-02-07 07:00:00+07	2025-02-12 07:11:25.778886+07	LG2500001	Logistics	0xc5a3b4d389b1eb2b197b12ed7b8e62e7c1211f32200c43ff80dc42542c96f91a
-EVENT-1739338141	Organic	QmYfvDGiBMDJLwG1xH4f4QzJWVhv2z2XkEZEdqRjwqon99	2026-06-01 07:00:00+07	2025-02-07 07:00:00+07	2025-02-12 12:29:01.757798+07	LG2500004	Logistics	0x3dd3d43aa72321edfc64f32ecd84e0372d297a581a399344d01fabd7d8e1c48e
-EVENT-1739558620	Organic	Qme83eJ6RWwGd3R3mvKd8JcW2S5nhqoagCno2frvrK7sdj	2026-06-01 07:00:00+07	2025-02-07 07:00:00+07	2025-02-15 01:43:40.913789+07	FA2500055	Farmer	0x289e574dfb86a668295506d0704b4d36a4520e929b6af15100c0207364542825
 \.
 
 
@@ -400,7 +405,7 @@ EVENT-1739558620	Organic	Qme83eJ6RWwGd3R3mvKd8JcW2S5nhqoagCno2frvrK7sdj	2026-06-
 -- Data for Name: retailer; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.retailer (retailerid, userid, companyname, telephone, createdon, address, city, province, country, postcode, email, lineid, facebook, location_link, username) FROM stdin;
+COPY public.retailer (retailerid, companyname, address, district, subdistrict, province, country, postcode, telephone, lineid, facebook, location_link, createdon, wallet_address, email) FROM stdin;
 \.
 
 
@@ -408,8 +413,8 @@ COPY public.retailer (retailerid, userid, companyname, telephone, createdon, add
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.users (userid, email, password, role, created_at, updated_at, deleted_at) FROM stdin;
-2500043	test@gmail.com	$2a$14$TIkY1keCg0iy2AtubF7xAuMud0SCx.pEVlrLoOwqgl3ppiWHIPCEq	farmer	2025-02-15 02:50:18.788822+07	2025-02-15 02:50:48.198377+07	0001-01-01 06:42:04+06:42:04
+COPY public.users (userid, username, email, password, role, entityid, created_at, updated_at, deleted_at) FROM stdin;
+2500048	Apiwat Bunyasartpan	farm@test.com	$2a$14$qskTlxk/N1wPXaNIqeGFZ.2PZLKQqZ7GoSt9bH6FzmELptQt3Z0w6	pending	PENDING_ROLE	2025-02-26 13:23:50.617784+07	2025-02-26 13:23:50.617784+07	0001-01-01 00:00:00
 \.
 
 
@@ -424,7 +429,7 @@ SELECT pg_catalog.setval('public.dairyfactory_id_seq', 24, true);
 -- Name: farmer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.farmer_id_seq', 56, true);
+SELECT pg_catalog.setval('public.farmer_id_seq', 59, true);
 
 
 --
@@ -445,7 +450,7 @@ SELECT pg_catalog.setval('public.retailer_id_seq', 3, true);
 -- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.user_id_seq', 43, true);
+SELECT pg_catalog.setval('public.user_id_seq', 48, true);
 
 
 --
@@ -472,14 +477,6 @@ ALTER TABLE ONLY public.dairyfactory
 
 
 --
--- Name: dairyfactory dairyfactory_userid_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.dairyfactory
-    ADD CONSTRAINT dairyfactory_userid_key UNIQUE (userid);
-
-
---
 -- Name: externalid externalid_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -501,14 +498,6 @@ ALTER TABLE ONLY public.farmer
 
 ALTER TABLE ONLY public.farmer
     ADD CONSTRAINT farmer_pkey PRIMARY KEY (farmerid);
-
-
---
--- Name: farmer farmer_userid_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.farmer
-    ADD CONSTRAINT farmer_userid_key UNIQUE (userid);
 
 
 --
@@ -560,19 +549,11 @@ ALTER TABLE ONLY public.retailer
 
 
 --
--- Name: logisticsprovider uni_logisticsprovider_userid; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: farmer uni_farmer_entityid; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.logisticsprovider
-    ADD CONSTRAINT uni_logisticsprovider_userid UNIQUE (userid);
-
-
---
--- Name: retailer uni_retailer_userid; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.retailer
-    ADD CONSTRAINT uni_retailer_userid UNIQUE (userid);
+ALTER TABLE ONLY public.farmer
+    ADD CONSTRAINT uni_farmer_entityid UNIQUE (entityid);
 
 
 --
@@ -592,46 +573,20 @@ ALTER TABLE ONLY public.users
 
 
 --
--- Name: dairyfactory dairyfactory_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.dairyfactory
-    ADD CONSTRAINT dairyfactory_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid) ON DELETE CASCADE;
-
-
---
--- Name: externalid externalid_factoryid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.externalid
-    ADD CONSTRAINT externalid_factoryid_fkey FOREIGN KEY (factoryid) REFERENCES public.dairyfactory(factoryid) ON DELETE CASCADE;
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_username_key UNIQUE (username);
 
 
 --
--- Name: farmer farmer_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: users set_timestamp; Type: TRIGGER; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.farmer
-    ADD CONSTRAINT farmer_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid) ON DELETE CASCADE;
-
-
---
--- Name: logisticsprovider logisticsprovider_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.logisticsprovider
-    ADD CONSTRAINT logisticsprovider_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid) ON DELETE CASCADE;
-
-
---
--- Name: retailer retailer_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.retailer
-    ADD CONSTRAINT retailer_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid) ON DELETE CASCADE;
+CREATE TRIGGER set_timestamp BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION public.update_timestamp();
 
 
 --
 -- PostgreSQL database dump complete
 --
-
